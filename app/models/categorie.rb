68ae0ffcaf774544cff38e61bdbc3c9e5d0eadef
Habib Payenda <habiburahmanpayenda@gmail.com>
2022-12-24 00:40:27 +0430

    Adds categorie model specs

--- app/models/categorie.rb
+++ app/models/categorie.rb
@@ -2,4 +2,11 @@ class Categorie < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :transaktion_categories
   has_many :transaktions, through: :transaktion_categories
+
+  validates :name, presence: true, length: { maximum: 25 }
+  validates :icon, presence: true, length: { maximum: 250 }
+
+  def name_with_initial
+    'habib'
+  end
 end
